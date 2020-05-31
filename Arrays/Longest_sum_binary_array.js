947807d77dd6f53da7662ce8f2f4f28f73309042
AmitJain628 <amitjain628@gmail.com>
2020-05-31 10:23:42 +0000

    Added code

--- Arrays/Longest_sum_binary_array.js
+++ Arrays/Longest_sum_binary_array.js
@@ -1,2 +1,27 @@
 
-console.log('hello');
+/*
+
+var arr[] = {1, 0, 1, 1, 1, 0, 0}
+Max = 0
+Sum[sum till i] = index;
+If same sum occur again compare max with current index - same index
+*/
+
+function getLongestSum(arr) {
+    var len = arr.length;
+    var max =0;
+    let sum = 0;
+    var map = {};
+    for(let i = 0; i < len; i++){
+        arr[i] === 0 ? sum += -1 : sum += 1;
+          
+        if(map[sum] === undefined) {
+          map[sum] = i;
+        } else {
+            max = Math.max(max, i - map[sum])
+        }
+    }
+
+    return max;
+}
+
