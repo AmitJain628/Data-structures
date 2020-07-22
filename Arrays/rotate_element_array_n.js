ef0d376146abf58f2dc3f9e3f303d4acd692b9a0
Amit jain <amitjain628@gmail.com>
2020-07-22 22:20:00 +0530

    Update rotate_element_array_n.js

--- Arrays/rotate_element_array_n.js
+++ Arrays/rotate_element_array_n.js
@@ -4,6 +4,8 @@ Program to rotate the element by N
 
 // Input arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2, n =7
 // Output arr[] = [3, 4, 5, 6, 7, 1, 2]
+// complexity -> O(n * d) and space -> O(1)
+For O(n) -> Check new juggling method by finding GCD and rotate the element only in split : https://www.geeksforgeeks.org/array-rotation/
 */
 
 var arr = [1, 2, 3, 4, 5, 6, 7];
