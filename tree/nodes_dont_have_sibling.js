80c2d3214968dd1d05a3ec166ab9e6d8455ce44b
Amit jain <amitjain628@gmail.com>
2020-10-14 15:29:13 +0530

    Update nodes_dont_have_sibling.js

--- tree/nodes_dont_have_sibling.js
+++ tree/nodes_dont_have_sibling.js
@@ -1,1 +1,22 @@
-a
+// https://www.geeksforgeeks.org/print-nodes-dont-sibling-binary-tree/
+// Complexity -> O(n) simple tree traversal
+
+let results =[];
+
+function printNodes(root){
+  
+if(root === null){
+  return null;
+}
+
+if(root.left !=null && root.right !=null){
+   printNodes(root.left);
+   printNodes(root.right);
+} else if(root.left === null) {
+    results.push(root.right);
+    printNodes(root.right);
+} else if(root.right === null) {
+    results.push(root.left);
+    printNodes(root.left);
+}
+}
