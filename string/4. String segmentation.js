3a10cca17c24fd9840e25b22f56d8f188e31c241
Amit jain <amitjain628@gmail.com>
2020-11-27 16:49:35 +0530

    Create 4. String segmentation.js

---
+++ string/4. String segmentation.js
@@ -0,0 +1,27 @@
+/*
+
+https://www.educative.io/m/string-segmentation
+
+*/
+
+function canStringSegmentation(str, arr){
+  for(let i=0; i<str.length; i++){
+    let first = str.subString(0, i);
+     if(arr.indexOf(first) !== -1){
+       let second = str.subString(i);
+       if(second.length === 0){
+          return true;
+       }
+       
+       if(arr.indexOf(second) !== -1){
+          return true;
+       }
+       
+       if(canStringSegmentation(second, arr)){
+          return true;
+       }
+     }
+  }
+
+  return false;
+}
