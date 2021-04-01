fc97a3ac9c1cd0a4721185a3c203d1c804b316d3
Amit jain <amitjain628@gmail.com>
2021-04-01 18:07:07 +0530

    Create debounce.js

---
+++ JS/debounce.js
@@ -0,0 +1,20 @@
+
+
+function debounce(func, wait, immediate) {
+  let timeout
+  
+  return function executedFunction() {
+    let context = this;
+    let args = arguments;
+    let later = function(){
+       if (!immediate) func.apply(context, args);
+    }
+     
+    let callNow = immediate && !timeout;
+    
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if(callNow) func.apply(context, args);
+ 
+  } 
+}
