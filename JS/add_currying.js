9c4e998429530aecc0d6106586730cc0a7200fa6
Amit jain <amitjain628@gmail.com>
2021-04-27 21:36:28 +0530

    Update add_currying.js

--- JS/add_currying.js
+++ JS/add_currying.js
@@ -11,3 +11,29 @@ function add() {
   }
   result();
 }
+
+
+function curry(fn) {
+    var args = Array.prototype.slice.call(arguments, 1);
+
+    return function () {
+        return fn.apply(this, args.concat(
+                Array.prototype.slice.call(arguments, 0)
+        ));
+    }
+}
+
+function add() {
+    var args = Array.prototype.slice.call(arguments);
+
+    return args.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue;
+    });
+}
+
+var curry1 = curry(add, 1);
+console.log(
+        curry1(2), // Logs 3
+        curry1(2, 3), // Logs 6
+        curry1(4, 5, 6) // Logs 16
+);
