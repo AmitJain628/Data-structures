0261ce36c839f373f35878eb1f87754f07bea394
Amit jain <amitjain628@gmail.com>
2021-06-15 12:51:58 +0530

    Update throttle.js

--- JS/throttle.js
+++ JS/throttle.js
@@ -45,3 +45,16 @@ function throttle(callback, wait, immediate = false) {
         }
       }
     }
+
+    
+    
+    function throttle(func, timeFrame) {
+  var lastTime = 0;
+  return function () {
+      var now = new Date();
+      if (now - lastTime >= timeFrame) {
+          func();
+          lastTime = now;
+      }
+  };
+}
